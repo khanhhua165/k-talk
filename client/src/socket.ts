8650ff69710cb28bae540884047a256cb8184205
khanhhua165 <billiejoe165@gmail.com>
2021-08-02 22:52:05 +0300

    removed logger

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,10 +1,6 @@
-import { SocketMore } from "./interfaces/socket.interface";
 import { API_URL } from "./constants/api";
 import { io } from "socket.io-client";
 
-const socket: SocketMore = io(API_URL, { autoConnect: false });
-
-//ANCHOR logging socket
-socket.onAny((event, ...args) => console.log(event, args));
+const socket = io(API_URL, { autoConnect: false });
 
 export default socket;
